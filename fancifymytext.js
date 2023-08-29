bf44cbdc656101e05e198a80e18245fa08c9316d
Brian <bdlowery2@gmail.com>
2023-08-29 15:26:51 -0600

    initial commit

---
+++ fancifymytext.js
@@ -0,0 +1,26 @@
+function helloWorld() {
+	const text = document.querySelector("#text");
+	text.style.fontSize = "24pt";
+}
+
+function fancy() {
+	const text = document.querySelector("#text");
+	text.style.fontWeight = "700";
+	text.style.color = "blue";
+	text.style.textDecoration = "underline";
+}
+
+function boring() {
+	const text = document.querySelector("#text");
+	text.style = "";
+}
+
+function moo() {
+	const text = document.querySelector("#text");
+	const upperCaseText = text.value.toUpperCase();
+
+	const splitUpper = upperCaseText.split(".");
+	text.value = splitUpper.join("-Moo");
+
+	// text.value = upperCaseText;
+}
